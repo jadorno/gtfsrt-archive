0ddd9ed7ca4fa45541f67d276cc9d1941aed63ea
Jennifer Adorno <jadorno@me.com>
2018-05-29 23:23:12 -0400

    Adding License and ReadMe

---
+++ README.md
@@ -0,0 +1,1 @@
+# tea-archive-pb
--- compress.py
+++ compress.py
@@ -12,7 +12,8 @@ delete_after_compress = True
 config = {}
 
 if os.environ.get('API_URL') is not None:
-	req = requests.get(os.environ.get('API_URL')+"/status/archive-config").json()
+	config['API_URL'] = os.environ.get('API_URL')
+	req = requests.get(config['API_URL']+"/status/archive-config").json()
 	config['DATA_NAME'] = os.environ.get('DATA_NAME', req['DATA_NAME'])
 	config['TIMEZONE'] = os.environ.get('TIMEZONE', req['TIMEZONE'])
 	config['PATH_PB'] = os.environ.get('PATH_PB', PB_INPUT_DIR)
@@ -24,6 +25,14 @@ else:
 	config['PATH_TAR'] = os.environ.get('PATH_TAR', TAR_OUTPUT_DIR)
 
 tz = pytz.timezone(config['TIMEZONE'])
+
+def notify(text):
+	if os.environ.get('API_URL') is not None:
+		payload = { 'header':{ 'timestamp' : int(dt.datetime.now(tz).timestamp()), 'tool' :'task-archive'}, 'data': text }
+		r = requests.post(os.environ.get('API_URL')+"/status/log-insert", json=payload)
+	print(text)
+
+notify("Archive Task Started")
 
 out_arrays = {}
 
@@ -47,7 +56,7 @@ for filename, files in sorted(out_arrays.items()):
 		print(filename+" "+str(len(files))+" files")
 
 		output_file = os.path.join(config['PATH_TAR'], filename+".tar.gz")
-		print("Creating: "+output_file)
+		notify("Creating: "+output_file)
 
 		if not os.path.isfile(output_file):
 			with tarfile.open(os.path.join(config['PATH_TAR'], filename+".tar.gz"), "w:gz") as tar:
@@ -58,8 +67,9 @@ for filename, files in sorted(out_arrays.items()):
 					os.remove(file)
 					print("Removed "+file)
 		else:
-			print(output_file+" already exists. This shouldn't be happening.")
+			notify(output_file+" already exists. This shouldn't be happening.")
 			exit()
 	else:
 		print(filename+".tar.gz not generated. Week ongoing.")
 
+notify("Archive Task Finished")
